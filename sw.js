82f242e9c8641396ca5eca72b6ca49eb54f2cb2a
ChemaBecerril <ChemaBecerril@users.noreply.github.com>
2024-04-01 13:25:56 +0000

    deploy: 98bbbc062cbe9db3074c9d63547f5d7f7192f354

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-be85a03dbffc26dec0c5.js"
+    "url": "webpack-runtime-357d2f04246c96d5cea7.js"
   },
   {
     "url": "framework-7752ef71ff976174a5db.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b0eeb264de0a12b0e838588a9ed1545d"
+    "revision": "7de7471311aba1ee28cc8be00b88336f"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-46b4173e1060a5c82475.js"
