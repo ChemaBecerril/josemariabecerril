cbbf40292be31df9a92102b92d458b3029f15a2b
ChemaBecerril <102516410+ChemaBecerril@users.noreply.github.com>
2024-07-28 13:57:50 +0200

    Update Footer.js
    Send to Linkedin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = (props) => (
                 <li><a href="#" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
                 <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
                 <li><a href="#" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                <li><a href="#" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                <li><a href="https://www.linkedin.com/in/josemaria-becerril-aceves/" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
             </ul>
             <ul className="copyright">
                 <li>&copy; www.josemariabecerril.com</li>
